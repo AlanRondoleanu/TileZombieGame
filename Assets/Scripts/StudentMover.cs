7eb73a5776863cb2939ee102c82afab9e9b62d44
Alan <megon12@gmail.com>
2023-01-12 13:49:00 +0000

    final

--- Assets/Scripts/StudentMover.cs
+++ Assets/Scripts/StudentMover.cs
@@ -7,6 +7,7 @@ public class StudentMover : MonoBehaviour
 {
     private NavMeshAgent agent;
     private bool panic = false;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
@@ -15,11 +16,34 @@ public class StudentMover : MonoBehaviour
         agent.updateRotation = false;
         agent.updateUpAxis = false;
         agent.speed = Random.Range(1.5f, 2.5f);
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Vector3 moveDirection = agent.velocity;
+
+        // Flip Image
+        if (moveDirection.x > 0)
+        {
+            Vector3 scale = transform.localScale;
+            scale.x = -2f;
+
+            transform.localScale = scale;
+        }
+        else if (moveDirection.x < 0)
+        {
+            Vector3 scale = transform.localScale;
+            scale.x = +2f;
+
+            transform.localScale = scale;
+        }
     }
 
     public void setTarget(Vector3 t_target)
     {
         agent.SetDestination(t_target);
+        animator.SetInteger("AnimState", 1);
     }
 
     public void runAway()
